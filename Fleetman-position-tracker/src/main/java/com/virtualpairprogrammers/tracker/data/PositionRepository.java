1adcfcdb3c5b225331e18c681ca9367c3b5ee1b7
Pavel <paveln@yahoo.com>
2020-07-26 16:16:59 -0400

    Added version for the Couchbase Spring library

--- Fleetman-position-tracker/src/main/java/com/virtualpairprogrammers/tracker/data/PositionRepository.java
+++ Fleetman-position-tracker/src/main/java/com/virtualpairprogrammers/tracker/data/PositionRepository.java
@@ -17,14 +17,9 @@ public interface PositionRepository extends CouchbasePagingAndSortingRepository<
 
 	TreeSet<VehiclePosition> findByNameAndTimestampAfterOrderByTimestampAsc(String name, Date timestamp);
 
-	List<VehiclePosition> findByName(String name);
-
 	List<VehiclePosition> findByNameOrderByTimestampAsc(String name);
 
 	List<VehiclePosition> findByTimestampAfter(Date since);
-
-	//@Query("SELECT * FROM #{#n1ql.bucket} WHERE #{#n1ql.filter} and name = $1 ORDER By timestamp ASC")
-	//List<VehiclePosition> findByNameAndSort(String name);
 
 
 }
